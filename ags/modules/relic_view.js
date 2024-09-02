c72ecc9867a7fe16a766073429854badccafa472
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2024-09-02 21:14:18 +1000

    Relic view sort

--- ags/modules/relic_view.js
+++ ags/modules/relic_view.js
@@ -84,6 +84,7 @@ const Relic = relic => {
         child: Box({ vertical: true }),
     });
     return Box({
+        attribute: relic,
         vertical: true,
         className: "relic",
         children: [RelicTitle(relic, revealer), revealer],
@@ -103,6 +104,96 @@ const FilterButton = ({ label, toggled = false, onToggled = () => {}, ...rest })
         },
         setup: setupCursorHover,
     });
+
+const SortChooser = (list, updateFilter) => {
+    const sorts = ["Default", "Platinum", "Ducats", "Platinum increase"];
+    const selected = Variable(sorts[0]);
+    let childFocused = false;
+
+    list.hook(selected, () => {
+        // Ignore init call
+        if (!list.children.length) return;
+
+        let children = null;
+        switch (selected.value) {
+            case "Default":
+                children = list.children.sort((a, b) => {
+                    a = a.attribute.tier + a.attribute.name;
+                    b = b.attribute.tier + b.attribute.name;
+                    return a > b ? 1 : a < b ? -1 : 0;
+                });
+                break;
+            case "Platinum":
+                children = list.children.sort(
+                    (a, b) => a.attribute.price.intact.platinum - b.attribute.price.intact.platinum
+                );
+                break;
+            case "Ducats":
+                children = list.children.sort(
+                    (a, b) => a.attribute.price.intact.ducats - b.attribute.price.intact.ducats
+                );
+                break;
+            case "Platinum increase":
+                children = list.children.sort(
+                    ({ attribute: { price: a } }, { attribute: { price: b } }) =>
+                        a.radiant.platinum - a.intact.platinum - (b.radiant.platinum - b.intact.platinum)
+                );
+                break;
+            default:
+                console.log(`[WARNING] Invalid sort choice: ${selected.value}`);
+                break;
+        }
+
+        if (children) {
+            list.children = children;
+
+            // Update filter cause changing sort messes it up
+            updateFilter();
+        }
+    });
+
+    const SortButton = sort =>
+        Button({
+            className: "relic-view-sort-button",
+            child: Label(sort),
+            onClicked: () => {
+                selected.value = sort;
+                revealer.revealChild = false;
+            },
+            setup: self => {
+                setupCursorHover(self);
+                self.on("focus-in-event", () => (childFocused = true));
+                self.on("focus-out-event", () => (childFocused = false));
+            },
+        });
+
+    const revealer = Revealer({
+        transition: "slide_right",
+        transitionDuration: 150,
+        revealChild: false,
+        child: Box({ children: selected.bind().as(s => sorts.filter(so => so !== s).map(SortButton)) }),
+    });
+
+    return Box({
+        className: "relic-view-sort-chooser",
+        children: [
+            Button({
+                child: Label({ label: selected.bind().as(s => `Sort: ${s}`) }),
+                onClicked: () => (revealer.revealChild = !revealer.revealChild),
+                setup: self => {
+                    setupCursorHover(self);
+                    self.on("focus-out-event", () =>
+                        // Idle so child listeners can trigger first then this
+                        Utils.idle(() => {
+                            if (!childFocused) revealer.revealChild = false;
+                        })
+                    );
+                },
+            }),
+            revealer,
+        ],
+    });
+};
 
 export default () => {
     let relicWidgets;
@@ -169,7 +260,7 @@ export default () => {
         children: [
             Box({
                 className: "relic-view-header",
-                children: [search, vaultedFilter],
+                children: [search, vaultedFilter, SortChooser(list, updateFilter)],
             }),
             Scrollable({
                 vexpand: true,
